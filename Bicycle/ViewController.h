b85bc2d9dedaa636797f7da448b6667bfdd75522
Sasha Zats <sasha@zats.io>
2014-07-05 19:12:12 +0300

    Initial Commit

---
+++ Bicycle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Bicycle
+//
+//  Created by Sasha Zats on 7/5/14.
+//  Copyright (c) 2014 Sash Zats. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
